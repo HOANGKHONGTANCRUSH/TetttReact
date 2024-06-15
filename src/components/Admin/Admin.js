bd8fe40f69d19ad380ba7eb11587d5400435c5fd
Viet Hoang <hoangpham.04042002@gmail.com>
2024-06-16 04:36:42 +0700

    Hoang 20

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,21 @@
+import SideBar from "./SideBar"
+import './Admin.scss';
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+
 const Admin = (props) => {
+    const [collapsed, setcollapsed] = useState(false)
     return (
-        <div>a components</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed}></SideBar>
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setcollapsed(!collapsed)} />
+                Hoang day
+            </div>
+        </div>
     )
 }
+
 export default Admin
