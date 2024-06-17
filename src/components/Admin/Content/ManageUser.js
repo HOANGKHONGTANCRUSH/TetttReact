dfaf22f3ad2b9dc666cf2bb52695c687d7f415ce
Viet Hoang <hoangpham.04042002@gmail.com>
2024-06-18 01:38:40 +0700

    Hoang 27

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,14 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './ManageUser.scss'
 import ModalCreateUser from './ModalCreateUser';
 import { FcPlus } from "react-icons/fc";
 import TableUser from './TableUser';
+import ModalUpdateUser from './ModalUpdateUser';
+import { getAllUsers } from "../../../services/apiSevice";
 
 
 
 const ManageUser = (props) => {
 
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+  const [showModalUpdateUser, setShowModalUpdateUser] = useState(false)
+  const [dataUpdate, setDataUpdate] = useState({})
+
+
+  const [listUsers, setlistUsers] = useState([])
+  useEffect(() => {
+    fetchListUsers();
+
+  }, []);
+
+  const fetchListUsers = async () => {
+    let res = await getAllUsers();
+    if (res.EC === 0) {
+      setlistUsers(res.DT)
+    }
+  }
+
+
+  const handleClickBtnUpdate = (user) => {
+    setShowModalUpdateUser(true)
+    setDataUpdate(user)
+    console.log('Ues :', user)
+  }
 
   return (
     <div className="manage-user-container">
@@ -20,10 +45,19 @@ const ManageUser = (props) => {
           <button className='btn btn-primary' onClick={() => setShowModalCreateUser(true)}><FcPlus />Add new users</button>
         </div>
         <div className='table-users-container'>
-          <TableUser />
+          <TableUser
+            listUsers={listUsers}
+            handleClickBtnUpdate={handleClickBtnUpdate}
+          />
         </div>
-        <ModalCreateUser show={showModalCreateUser}
+        <ModalCreateUser
+          show={showModalCreateUser}
           setShow={setShowModalCreateUser}
+        />
+        <ModalUpdateUser
+          show={showModalUpdateUser}
+          setShow={setShowModalUpdateUser}
+          dataUpdate={dataUpdate}
         />
       </div>
     </div>
